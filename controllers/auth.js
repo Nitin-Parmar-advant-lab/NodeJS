d4769c8228f9f0b4d47f9f137e5d6998e107dfcb
1-and-ME <nitin.oneand@gmail.com>
2026-05-25 14:48:26 +0530

    mail sending through resend

---
+++ controllers/auth.js
@@ -0,0 +1,113 @@
+const bcrypt = require('bcryptjs');
+const { Resend } = require('resend');
+
+const User = require('../models/user');
+
+const resend = new Resend(process.env.RESEND_API);
+
+exports.getLogin = (req, res, next) => {
+  let message = req.flash('error');
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
+  res.render('auth/login', {
+    path: '/login',
+    pageTitle: 'Login',
+    errorMessage: message
+  });
+};
+
+exports.getSignup = (req, res, next) => {
+  let message = req.flash('error');
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
+  res.render('auth/signup', {
+    path: '/signup',
+    pageTitle: 'Signup',
+    errorMessage: message
+  });
+};
+
+exports.postLogin = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  User.findOne({ email: email })
+    .then(user => {
+      if (!user) {
+        req.flash('error', 'Invalid email or password.');
+        return res.redirect('/login');
+      }
+      bcrypt
+        .compare(password, user.password)
+        .then(doMatch => {
+          if (doMatch) {
+            req.session.isLoggedIn = true;
+            req.session.user = user;
+            return req.session.save(err => {
+              console.log(err);
+              res.redirect('/');
+            });
+          }
+          req.flash('error', 'Invalid email or password.');
+          res.redirect('/login');
+        })
+        .catch(err => {
+          console.log(err);
+          res.redirect('/login');
+        });
+    })
+    .catch(err => console.log(err));
+};
+
+exports.postSignup = (req, res, next) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const confirmPassword = req.body.confirmPassword;
+  User.findOne({ email: email })
+    .then(userDoc => {
+      if (userDoc) {
+        req.flash('error', 'E-Mail exists already, please pick a different one.');
+        return res.redirect('/signup');
+      }
+      return bcrypt
+        .hash(password, 12)
+        .then(hashedPassword => {
+          const user = new User({
+            email: email,
+            password: hashedPassword,
+            cart: { items: [] }
+          });
+          return user.save();
+        })
+        .then(result => {
+          // Send welcome email
+          resend.emails.send({
+            from: 'onboarding@resend.dev',
+            to: email,
+            subject: 'Welcome to Our App!',
+            html: `<p>Hello,</p>
+                   <p>Thank you for signing up! Welcome to our application.</p>
+                   <p>Your account has been successfully created with email: <strong>${email}</strong></p>
+                   <p>You can now <a href="http://localhost:3000/login">login to your account</a>.</p>
+                   <p>Best regards,<br>The Team</p>`
+          }).catch(err => console.log('Email sending error:', err));
+          
+          res.redirect('/login');
+        });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy(err => {
+    console.log(err);
+    res.redirect('/');
+  });
+};
